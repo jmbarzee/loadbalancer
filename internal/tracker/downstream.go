468537bfacfa3720b3854f244a242947078bec3e
Jacob Barzee <jmbarzee@gmail.com>
2024-04-11 12:43:35 -0500

    update terminology for downstreams to reference IDs for clarity

--- internal/tracker/downstream.go
+++ internal/tracker/downstream.go
@@ -11,7 +11,7 @@ type DownstreamConns struct {
 	// mu protects the resources of DownstreamConns
 	mu sync.Mutex
 
-	// connCounts is a map of downstream to a count of connections
+	// connCounts is a map of downstreamID to a count of connections
 	connCounts map[string]uint32
 }
 
@@ -22,30 +22,30 @@ func NewDownstreamConns() *DownstreamConns {
 	}
 }
 
-// TryBeginConnection checks if a downstream is below a maximum
-// and if so records an additional connection for the client.
-// If the client has no history, a new count will be started.
+// TryBeginConnection checks if a DownstreamID is below a maximum
+// and if so records an additional connection for the downstream.
+// If the downstream has no history, a new count will be started.
 // The return indicates if the new connection should be allowed.
-func (t *DownstreamConns) TryBeginConnection(downstream string, max uint32) bool {
+func (t *DownstreamConns) TryBeginConnection(DownstreamID string, max uint32) bool {
 	t.mu.Lock()
 	defer t.mu.Unlock()
-	value, ok := t.connCounts[downstream]
+	value, ok := t.connCounts[DownstreamID]
 	if !ok {
-		t.connCounts[downstream] = 1
+		t.connCounts[DownstreamID] = 1
 		return true
 	}
 	if value < max {
-		t.connCounts[downstream] = value + 1
+		t.connCounts[DownstreamID] = value + 1
 		return true
 	}
 	return false
 }
 
-// EndConnection decrements the count of connections for a given downstream.
+// EndConnection decrements the count of connections for a given DownstreamID.
 // EndConnection requires that a connection was begun previously,
 // otherwise it may access a key which does not exist.
-func (t *DownstreamConns) EndConnection(downstream string) {
+func (t *DownstreamConns) EndConnection(DownstreamID string) {
 	t.mu.Lock()
 	defer t.mu.Unlock()
-	t.connCounts[downstream] -= t.connCounts[downstream]
+	t.connCounts[DownstreamID] -= t.connCounts[DownstreamID]
 }
